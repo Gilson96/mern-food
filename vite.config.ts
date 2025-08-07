2084d5ec64da47c3a4a3952cc46fd8a399787eda
Gilson96 <grafael99@gmail.com>
2025-08-07 18:02:33 +0100

    Fixed deployement setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/mern-food/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
